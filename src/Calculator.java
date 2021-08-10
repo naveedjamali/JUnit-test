b8ee33bbcfe95e1a81d26b03c06f378a3fc0978c
Naveed Jamali <naveedjamali@outlook.com>
2021-08-10 23:55:31 +0500

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static void main(String[] args) {
+        Calculator c =new Calculator();
+        System.out.println(c.add(2,3));
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+}
